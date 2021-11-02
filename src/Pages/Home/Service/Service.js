e42f5088b1cc5e710fae782f7507c3b2a44289cd
Suhag Al Amin <developersuhag@gmail.com>
2021-11-02 14:51:30 +0600

    service section

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,10 +1,27 @@
+import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import React from "react";
 
-const Service = () => {
+const Service = ({ service }) => {
+  const { serviceName, description, img } = service;
   return (
-    <div>
-      <h3>single servcie</h3>
-    </div>
+    <Grid item xs={4} sm={4} md={4}>
+      <Card sx={{ maxWidth: 345, border: 0, boxShadow: 0, py: 2 }}>
+        <CardMedia
+          sx={{ width: 70, mx: "auto" }}
+          component="img"
+          image={img}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {serviceName}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 };
 
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,19 +1,54 @@
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import React from "react";
+import fluoride from "../../../images/fluoride.png";
+import cavity from "../../../images/cavity.png";
+import whitening from "../../../images/whitening.png";
+import Service from "../Service/Service";
+
+const services = [
+  {
+    serviceName: "Fluoride Treatment",
+    description:
+      "Fluoride treatments are typically professional treatments containing a high concentration of fluoride that a dentist or hygienist will apply to a person's teeth to improve health and reduce the risk of cavities.",
+    img: fluoride,
+  },
+  {
+    serviceName: "Cavity Filling",
+    description:
+      "Having a cavity filled may cause some discomfort, but it should not cause pain. Anyone who experiences moderate or severe pain during or after the procedure should let their dentist know.",
+    img: cavity,
+  },
+  {
+    serviceName: "Teath Whitening",
+    description:
+      "It's never been easier to brighten your smile at home. There are all kinds of products you can try: rinses, gels, chewing gum, toothpastes, and strips.",
+    img: whitening,
+  },
+];
 
 const Services = () => {
   return (
-    <Container>
+    <Container sx={{ py: 6, textAlign: "center" }}>
+      <Typography
+        sx={{ textAlign: "center", my: 1, color: "#5FC7C7" }}
+        variant="h5"
+      >
+        OUR SERVICES
+      </Typography>
+      <Typography
+        sx={{ textAlign: "center", my: 2, fontWeight: 700 }}
+        variant="h4"
+      >
+        Services We Provide
+      </Typography>
       <Grid
         container
         spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
       >
-        {
-          <Grid item xs={4} sm={4} md={4}>
-            <h3>hello</h3>
-          </Grid>
-        }
+        {services.map((service) => (
+          <Service service={service}></Service>
+        ))}
       </Grid>
     </Container>
   );
